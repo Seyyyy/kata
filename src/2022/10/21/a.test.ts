76234d2ba9606db17e922a90ec26f285f7ba071e
Seyyyy <mysdc7@gmail.com>
2022-10-21 07:19:49 +0900

    resolve

--- src/2022/10/21/a.test.ts
+++ src/2022/10/21/a.test.ts
@@ -1,5 +1,21 @@
 import { func } from "./a";
 
-describe("", () => {
-  test.todo("");
+describe("f(k) = k * f(k - 1)", () => {
+  test("f(0) = 1", () => {
+    const result = func(0);
+    expect(result).toBe(1);
+  });
+
+  test("f(2)", () => {
+    const result = func(2);
+    expect(result).toBe(2);
+  });
+  test("f(3)", () => {
+    const result = func(3);
+    expect(result).toBe(6);
+  });
+  test("f(10)", () => {
+    const result = func(10);
+    expect(result).toBe(3628800);
+  });
 });
--- src/2022/10/21/a.ts
+++ src/2022/10/21/a.ts
@@ -1,3 +1,6 @@
-export const func = () => {
-  return 1;
+export const func = (k: number): number => {
+  if (k == 0) {
+    return 1;
+  }
+  return k * func(k - 1);
 };
